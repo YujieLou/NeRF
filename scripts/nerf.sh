7274458fb35cda77e6d3c2069f0ca762acab38c9
YujieLou <yjlou@alumni.tongji.edu.cn>
2023-07-03 12:46:48 +0000

    tt0703

--- scripts/nerf.sh
+++ scripts/nerf.sh
@@ -1,1 +1,4 @@
 python -m torch.distributed.launch --nproc_per_node 1 --master_port 60660 main_ddp.py configs/fern.txt --local_rank 3
+
+
+python -m torch.distributed.launch --nproc_per_node 1 --master_port 60002 main_ddp.py configs/A-good.txt --local_rank 2
